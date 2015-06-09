25b7e108aee64a15351689e38601d32f385528f0
MohamedBassem <medoox240@gmail.com>
2015-06-09 23:03:18 +0200

    Adding some spam referrals

--- spam-referrals-blocker.js
+++ spam-referrals-blocker.js
@@ -1,6 +1,15 @@
 function isSpamReferral(){
   var spamReferrals = [
-                      ];
+    /buttons-for-website\.com/,
+    /social-buttons\.com/,
+    /4webmasters\.org/,
+    /semalt\.com/,
+    /free-share-buttons\.com/,
+    /best-seo-offer\.com/,
+    /trafficmonetize\.org/,
+    /100dollars-seo\.com/,
+    /event-tracking\.com/,
+  ];
   var currentReferral = document.referrer;
   if(currentReferral){
     for(var i=0; i< spamReferrals.length;i++){
