e7929296ca97f7674be2ccf0c2deede44e224be1
MohamedBassem <medoox240@gmail.com>
2015-06-29 16:55:12 +0200

    Redirecting the curl output to the release folder

--- script.sh
+++ script.sh
@@ -2,5 +2,6 @@
 
 cat referrer-spam-blacklist/spammers.txt | sed 's/\./\\./g' | sed -r 's/^(.*)$/\t\/\0\/,/' > tmp.tmp
 sed '/.*\/\/.*\$\$REPLACE_ME\$\$/r tmp.tmp' spam-referrals-blocker-template.js > tmp2.tmp
-curl -X POST -s --data-urlencode 'input@tmp2.tmp' http://javascript-minifier.com/raw
+curl -X POST -s --data-urlencode 'input@tmp2.tmp' http://javascript-minifier.com/raw > ./release/spam-referrals-blocker.js.min
 rm tmp.tmp tmp2.tmp
+cat ./release/spam-referrals-blocker.js.min
