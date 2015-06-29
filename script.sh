06198e727572232b4e1666718872ce83dadad87f
MohamedBassem <medoox240@gmail.com>
2015-06-29 16:51:45 +0200

    Adding the minifying script

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat referrer-spam-blacklist/spammers.txt | sed 's/\./\\./g' | sed -r 's/^(.*)$/\t\/\0\/,/' > tmp.tmp
+sed '/.*\/\/.*\$\$REPLACE_ME\$\$/r tmp.tmp' spam-referrals-blocker-template.js > tmp2.tmp
+curl -X POST -s --data-urlencode 'input@tmp2.tmp' http://javascript-minifier.com/raw
+rm tmp.tmp tmp2.tmp
