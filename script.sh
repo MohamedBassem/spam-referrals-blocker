0016e020ddc95ff42321434462fb94c48d4635bf
Quinten Krijger <qkrijger@gmail.com>
2016-04-10 14:44:28 +0200

    Script now updates the Git submodule with the referrer list
    Signed-off-by: Quinten Krijger <qkrijger@gmail.com>

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+git submodule update --remote
 cat referrer-spam-blacklist/spammers.txt | sed 's/\./\\./g' | sed -r 's/^(.*)$/\t\/\0\/,/' > tmp.tmp
 sed '/.*\/\/.*\$\$REPLACE_ME\$\$/r tmp.tmp' spam-referrals-blocker-template.js > tmp2.tmp
 curl -X POST -s --data-urlencode 'input@tmp2.tmp' http://javascript-minifier.com/raw > ./release/spam-referrals-blocker.min.js
 rm tmp.tmp tmp2.tmp
 cat ./release/spam-referrals-blocker.js.min
+
