f8fadd4d8dec068ae4f00f2612e2c7a7b2535ce2
MohamedBassem <medoox240@gmail.com>
2016-04-11 09:31:23 +0200

    Fixing the script

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ cat referrer-spam-blacklist/spammers.txt | sed 's/\./\\./g' | sed -r 's/^(.*)$/\
 sed '/.*\/\/.*\$\$REPLACE_ME\$\$/r tmp.tmp' spam-referrals-blocker-template.js > tmp2.tmp
 curl -X POST -s --data-urlencode 'input@tmp2.tmp' http://javascript-minifier.com/raw > ./release/spam-referrals-blocker.min.js
 rm tmp.tmp tmp2.tmp
-cat ./release/spam-referrals-blocker.js.min
+cat ./release/spam-referrals-blocker.min.js
 
